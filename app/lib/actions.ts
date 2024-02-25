7451403fb97b011f9ce556c9f92a3f03ee3a15b9
thadeus47 <52805067+thadeus47@users.noreply.github.com>
2024-02-25 23:45:10 +0300

    changes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,12 +7,18 @@ import { redirect } from 'next/navigation';
 
 
 const FormSchema = z.object({
-    id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
-    date: z.string(),
-  });
+  id: z.string(),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
+  date: z.string(),
+});
    
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
