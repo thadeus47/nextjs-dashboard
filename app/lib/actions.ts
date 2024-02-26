0fc7cac9756d068e8c1c831e219ddf95b8be0ef1
thadeus47 <52805067+thadeus47@users.noreply.github.com>
2024-02-26 23:57:22 +0300

    changes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -50,7 +50,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
    const { customerId, amount, status } = validatedFields.data;
    const amountInCents = amount * 100;
    const date = new Date().toISOString().split('T')[0];
-   
+
    // Insert data into the database
     try {
       await sql`
@@ -71,12 +71,20 @@ export async function createInvoice(prevState: State, formData: FormData) {
  
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
