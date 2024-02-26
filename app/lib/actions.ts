fc833480311fe708e38a3e6afc8790684bbc138e
thadeus47 <52805067+thadeus47@users.noreply.github.com>
2024-02-27 00:02:52 +0300

    accessibility actions update

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,12 +69,9 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 }
  
-export async function updateInvoice(
-  id: string,
-  prevState: State,
-  formData: FormData,
-) {
-  const validatedFields = UpdateInvoice.safeParse({
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+export async function updateInvoice(id: string, formData: FormData) {
+   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
@@ -86,16 +83,16 @@ export async function updateInvoice(
       message: 'Missing Fields. Failed to Update Invoice.',
     };
   }
- 
-  const { customerId, amount, status } = validatedFields.data;
+
+   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
  
   try {
     await sql`
-      UPDATE invoices
-      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-      WHERE id = ${id}
-    `;
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
